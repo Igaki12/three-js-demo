266c4f812a85dd68e6934708e5f5c412891b0a19
Igaki12 <igakin8.19971222@gmail.com>
2024-12-22 18:57:28 +0900

    諦めてカメラ動かす練習

--- main.js
+++ main.js
@@ -21,71 +21,28 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 // renderer.setAnimationLoop( animate );
+
+// cameraを任意に動かせるようにする: https://qiita.com/dsudo/items/42da9c7f45ed6147395e
+renderer.setPixelRatio( window.devicePixelRatio );
 document.body.appendChild( renderer.domElement );
 
-// 単純なcubeを表示
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
+単純なcubeを表示
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-// camera.position.z = 5;
+camera.position.z = 5;
 
-// function animate() {
+function animate() {
 
-// 	cube.rotation.x += 0.01;
-// 	cube.rotation.y += 0.01;
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
-// 	renderer.render( scene, camera );
+	renderer.render( scene, camera );
 
-// }
-
-// https://threejs.org/docs/#api/en/objects/SkinnedMesh からコピペして3Dモデルを表示
-
-const geometry = new THREE.CylinderGeometry( 5, 5, 5, 5, 15, 5, 30 );
-
-// create the skin indices and skin weights manually
-// (typically a loader would read this data from a 3D model for you)
-
-const position = geometry.attributes.position;
-
-const vertex = new THREE.Vector3();
-
-const skinIndices = [];
-const skinWeights = [];
-
-for ( let i = 0; i < position.count; i ++ ) {
-
-	vertex.fromBufferAttribute( position, i );
-
-	// compute skinIndex and skinWeight based on some configuration data
-	const y = ( vertex.y + sizing.halfHeight );
-	const skinIndex = Math.floor( y / sizing.segmentHeight );
-	const skinWeight = ( y % sizing.segmentHeight ) / sizing.segmentHeight;
-	skinIndices.push( skinIndex, skinIndex + 1, 0, 0 );
-	skinWeights.push( 1 - skinWeight, skinWeight, 0, 0 );
 }
+renderer.render( scene, camera );
 
-geometry.setAttribute( 'skinIndex', new THREE.Uint16BufferAttribute( skinIndices, 4 ) );
-geometry.setAttribute( 'skinWeight', new THREE.Float32BufferAttribute( skinWeights, 4 ) );
 
-// create skinned mesh and skeleton
 
-const mesh = new THREE.SkinnedMesh( geometry, material );
-const skeleton = new THREE.Skeleton( bones );
-
-// see example from THREE.Skeleton
-const rootBone = skeleton.bones[ 0 ];
-mesh.add( rootBone );
-
-// bind the skeleton to the mesh
-mesh.bind( skeleton );
-
-// move the bones and manipulate the model
-skeleton.bones[ 0 ].rotation.x = -0.1;
-skeleton.bones[ 1 ].rotation.x = 0.2;
-
-scene.add(mesh);
-
-camera.position.z = 15;
- renderer.render( scene, camera );
