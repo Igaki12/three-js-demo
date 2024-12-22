6abb9ece993c94310c81649b30a1fe3cd4799b58
Igaki12 <igakin8.19971222@gmail.com>
2024-12-22 11:49:59 +0900

    筋肉3Dモデルを読み込む

--- main.js
+++ main.js
@@ -10,6 +10,7 @@
 //     }
 //   }
 
+import { render } from 'react-dom';
 import * as THREE from 'three';
 // import { THREE } from 'https://cdn.jsdelivr.net/npm/three@0.171.0/build/three.module.js'; 
 
@@ -23,18 +24,56 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+// camera.position.z = 5;
+
+// function animate() {
+
+// 	cube.rotation.x += 0.01;
+// 	cube.rotation.y += 0.01;
+
+// 	renderer.render( scene, camera );
+
+// }
+
+// https://threejs.org/docs/#examples/en/loaders/MMDLoader からMuscle3Dモデルを読み込む
+import { MMDLoader } from 'three/addons/loaders/MMDLoader.js';
+
+// Instantiate a loader
+const loader = new MMDLoader();
+
+// Load a MMD model
+loader.load(
+	// path to PMD/PMX file
+	'muscle3D.pmx',
+	// called when the resource is loaded
+	function ( mesh ) {
+
+		scene.add( mesh );
+
+	},
+	// called when loading is in progress
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
 
 camera.position.z = 5;
-
-function animate() {
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-
+function animate () {
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.01;
 }
+renderer.render( scene, camera );
+
